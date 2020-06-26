108749145b8e628c6f070be3149ff42749bc1567
ödutolaabisoye <dutolaabisoye@gmail.com>
2020-06-26 06:47:30 +0100

    Setting Stateful Widget

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+      MaterialApp(
+          home: Scaffold(
+            backgroundColor: Colors.red,
+            appBar: AppBar(
+              centerTitle: true,
+              title: Text('Dicee'),
+              backgroundColor: Colors.red,
+            ),
+            body: DicePage(),
+          ),
+      ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 5;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+                onPressed: (){
+                  setState((){
+                    leftDiceNumber = 3;
+                  });
+                  print('Left button got pressed!');
+                },
+                child: Image.asset('images/dice$leftDiceNumber.png')
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                print('Right button got pressed!');
+              },
+              child: Image.asset('images/dice2.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
